728ec0db4d5c4dd7e47a125ffb044863777f964b
vijay2181 <66196388+vijay2181@users.noreply.github.com>
2022-09-09 15:40:45 +0530

    Update iamrole.tf

--- iamrole.tf
+++ iamrole.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "ecr_access_role" {
+resource "aws_iam_role" "access_role" {
   name               = "Instance-Role"
   assume_role_policy = file("./policy/assumerolepolicy.json")
 }
@@ -11,12 +11,12 @@ resource "aws_iam_policy" "policy-1" {
 
 resource "aws_iam_policy_attachment" "policy-attach1" {
   name       = "policy-attachment1"
-  roles      = ["${aws_iam_role.ecr_access_role.name}"]
+  roles      = ["${aws_iam_role.access_role.name}"]
   policy_arn = aws_iam_policy.policy-1.arn
 }
 
 
 resource "aws_iam_instance_profile" "vijay_profile" {
   name = "vijay_profile"
-  role = aws_iam_role.ecr_access_role.name
+  role = aws_iam_role.access_role.name
 }
